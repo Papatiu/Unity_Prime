7a7086ac88d7902a82794581d3ef37bd6a63b70e
Papatiu <osman.zaman2012@gmail.com>
2025-08-01 23:00:18 +0300

    Update config_manager.py

---
+++ utils/config_manager.py
@@ -0,0 +1,12 @@
+# utils/config_manager.py
+# Tüm ayarlarımızı buradan yöneteceğiz.
+
+# GitHub'dan aldığın Personal Access Token (PAT)
+
+# Postacı Sunucu'nun Unity'ye bağlanacağı adres
+UNITY_CLIENT_IP = "127.0.0.1"
+UNITY_CLIENT_PORT = 6400
+
+# Bizim Postacı Sunucumuzun dinleyeceği adres (Arayüz buraya bağlanır)
+UNITY_SERVER_IP = "127.0.0.1"
+UNITY_SERVER_PORT = 8080
